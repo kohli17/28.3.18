cf4b3cbb6bd95c4f7d005240ed73dadfbf2dd6f3
kohli17 <35589866+kohli17@users.noreply.github.com>
2018-03-28 16:12:07 +0530

    Create 3 character.c
    print the string

---
+++ 3 character.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include<string.h>
+#include<conio.h>
+int main(void) 
+{
+	char s[30];
+	int i,l;
+	gets(s);
+	l=strlen(s);
+	
+	for(i=0;i<l;i=i+3)
+	{
+		printf("%c",s[i]);
+	}
+	return 0;
+}
